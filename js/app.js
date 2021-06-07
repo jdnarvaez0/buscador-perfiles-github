56cf6fba99e1fb7cf78566b17754c66fcdc91822
navidev0 <juand.ns07@gmail.com>
2021-06-07 19:25:02 -0300

    funciones crearErrorCard, agregarRepos, agregarEventos al submit

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 const APIURL = 'https://api.github.com/users/'
-// console.log('https://api.github.com/users/navidev0');
+
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
-const seach = document.getElementById('buscador');
+const search = document.getElementById('buscador');
 
 async function getUser(username) {
  try {
-  const { dato } = await axios(APIURL + username); 
+  const { data } = await axios(APIURL + username); 
   
-  crearCardUsuario(dato);
+  crearCardUsuario(data);
   getRepos(username)
- } catch (error) {
-   if (error.response.status == 404) {
+ } catch (err) {
+   if (err.response.status == 404) {
     crearErrorCard('No se encontro perfil con este nombre de usuario');
    }
  } 
@@ -20,11 +20,11 @@ async function getUser(username) {
 
 async function getRepos(username) {
   try {
-    const { data } = await axios(APIURL + username + '/repos?sort=created');
+    const { data } = await axios(APIURL + username + '/repos?sort=created')
 
     agregarReposAlCard(data);
-  } catch (error) {
-   crearErrorCard('Probelmas con la obtecion de los repos') 
+  } catch (err) {
+   crearErrorCard('Problemas con la obtecion de los repos') 
   }
 }
 
@@ -50,5 +50,37 @@ function crearCardUsuario(user) {
   main.innerHTML = cardHTML;
 }
 
+function crearErrorCard(msg) {
+  const cardHTML =`
+    <div class="card">
+      <h1>${msg}</h1>
+    </div>
+  `
+  main.innerHTML= cardHTML;
+}
 
+function agregarReposAlCard(repos) {
+  const reposEl = document.getElementById('repos')
+
+  repos.slice(0, 5).forEach(repo => {
+    const repoEl = document.createElement('a');
+    repoEl.classList.add('repo');
+    repoEl.href = repo.html_url;
+    repoEl.targe = 'blank';
+    repoEl.innerHTML = repo.name;
+
+    reposEl.appendChild(repoEl);
+  });
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const user = search.value
+
+  if(user) {
+      getUser(user)
+
+      search.value = ''
+  }
+});
 
